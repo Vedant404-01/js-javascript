f0c5fcb3f18cbeeb3f82780076d703eff599ec81
Vedant A Nannavare <115454886+Vedant404-01@users.noreply.github.com>
2024-09-19 17:21:53 +0000

    datatype conversion

---
+++ 01_basics/Datatype_conversion.js
@@ -0,0 +1,10 @@
+// let num = 33
+// console.log(typeof num);
+// console.log(num);
+
+/* Convert num into string*/  
+
+let num = 33
+let Number = String(num)
+console.log(typeof Number);
+console.log(Number);
---
+++ 01_basics/variable_datatype.js
@@ -0,0 +1,19 @@
+const name = "Vedant"
+let Email= "vedant@google.com"
+let EmailPassword= 1234
+
+console.table([name,Email,EmailPassword]);
+// var shouldn't be used in order to avoid issue of block scope and functional scope.
+
+// -------------------------------------DATATYPES--------------------------------//
+
+/* 
+Number => Range is 2 power of 53
+String => ""
+BigInt
+Boolean => True or False
+null => Object
+undefined => undefined
+Symbol => Unique
+Object => 
+*/
